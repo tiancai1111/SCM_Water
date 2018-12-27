ff21393dacdde7c756478be75efd43f9a1c46b40
liuxun <1244594831@qq.com>
2018-12-27 16:32:15 +0800

    123

--- SCM_Water_frontDesk/src/main/java/controller/IndexController.java
+++ SCM_Water_frontDesk/src/main/java/controller/IndexController.java
@@ -1,14 +1,16 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
    @RequestMapping("hhhh")
    public String hhhh() {
-	return "index";	   
+	return "login";	   
+   }
+   @RequestMapping("gggg")
+   public String gggg() {
+	return "register";	   
    }
 }
