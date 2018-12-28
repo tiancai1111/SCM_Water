16b54beb3e118859c507d08ba1a81bb559fa74b9
liuxun <1244594831@qq.com>
2018-12-28 18:55:05 +0800

    32423424242342

--- SCM_Water_frontDesk/src/main/java/controller/IndexController.java
+++ SCM_Water_frontDesk/src/main/java/controller/IndexController.java
@@ -1,14 +1,19 @@
 package controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import mapper.UserMapper;
 import pojo.User;
 
 @Controller
 public class IndexController {
+	@Resource
+	private UserMapper userMapper;
    @RequestMapping("hhhh")
    public String hhhh() {
 	return "login";	   
@@ -20,6 +25,7 @@ public class IndexController {
    @ResponseBody
    @PostMapping("qqqq")
    public String qqqq(User user) {
+	   user.setName("张三");
 	   System.out.println(user.getPhone());
 	return "register";	   
    }
