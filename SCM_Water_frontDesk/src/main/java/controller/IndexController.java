32b0e73c07cef69eb5da5c540340d0b39c1f2e47
liuxun <1244594831@qq.com>
2018-12-28 09:00:12 +0800

    注册1号

--- SCM_Water_frontDesk/src/main/java/controller/IndexController.java
+++ SCM_Water_frontDesk/src/main/java/controller/IndexController.java
@@ -1,7 +1,11 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import pojo.User;
 
 @Controller
 public class IndexController {
@@ -13,4 +17,11 @@ public class IndexController {
    public String gggg() {
 	return "register";	   
    }
+   @ResponseBody
+   @PostMapping("qqqq")
+   public String qqqq(User user) {
+	   System.out.println(user.getPhone());
+	return "register";	   
+   }
+
 }
